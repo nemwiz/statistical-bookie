b6ebc9bed17656196ead252cce9fac0ba41f16f5
nemwiz <s3ptease@yahoo.com>
2016-11-12 17:46:01 +0100

    Implemented match outcome view and aggregator

--- backend/src/main/java/aggregator/MatchDetailOutcomeAggregator.java
+++ backend/src/main/java/aggregator/MatchDetailOutcomeAggregator.java
@@ -5,6 +5,8 @@ import viewmodel.MatchDetailOutcomeView;
 
 import java.util.List;
 
+import static helper.Constants.*;
+
 public class MatchDetailOutcomeAggregator extends Aggregator {
 
     private List<Match> matches;
@@ -15,6 +17,83 @@ public class MatchDetailOutcomeAggregator extends Aggregator {
 
     @Override
     public MatchDetailOutcomeView getAggregatedCount() {
-        return null;
+        return new MatchDetailOutcomeView(
+                countWhenHomeTeamWonOnHalfTimeAndWonInTheEnd(),
+                countWhenHomeTeamWonOnHalfTimeAndDrewInTheEnd(),
+                countWhenHomeTeamWonOnHalfTimeAndLostInTheEnd(),
+                countWhenHalfTimeWasDrawAndHomeTeamWonInTheEnd(),
+                countWhenHalfTimeWasDrawAndWasDrawInTheEnd(),
+                countWhenHalfTimeWasDrawAndAwayTeamWonInTheEnd(),
+                countWhenAwayTeamWonOnHalfTimeAndWonInTheEnd(),
+                countWhenAwayTeamWonOnHalfTimeAndDrewInTheEnd(),
+                countWhenAwayTeamWonOnHalfTimeAndHomeTeamWonInTheEnd()
+        );
+    }
+
+    private long countWhenHomeTeamWonOnHalfTimeAndWonInTheEnd() {
+
+        return matches.stream()
+                .filter(match -> getMatchPredicate(match, HOME_TEAM_WIN, HOME_TEAM_WIN))
+                .count();
+    }
+
+    private long countWhenHomeTeamWonOnHalfTimeAndDrewInTheEnd() {
+
+        return matches.stream()
+                .filter(match -> getMatchPredicate(match, HOME_TEAM_WIN, DRAW))
+                .count();
+    }
+
+    private long countWhenHomeTeamWonOnHalfTimeAndLostInTheEnd() {
+
+        return matches.stream()
+                .filter(match -> getMatchPredicate(match, HOME_TEAM_WIN, AWAY_TEAM_WIN))
+                .count();
+    }
+
+    private long countWhenHalfTimeWasDrawAndHomeTeamWonInTheEnd() {
+
+        return matches.stream()
+                .filter(match -> getMatchPredicate(match, DRAW, HOME_TEAM_WIN))
+                .count();
+    }
+
+    private long countWhenHalfTimeWasDrawAndWasDrawInTheEnd() {
+
+        return matches.stream()
+                .filter(match -> getMatchPredicate(match, DRAW, DRAW))
+                .count();
+    }
+
+    private long countWhenHalfTimeWasDrawAndAwayTeamWonInTheEnd() {
+
+        return matches.stream()
+                .filter(match -> getMatchPredicate(match, DRAW, AWAY_TEAM_WIN))
+                .count();
+    }
+
+    private long countWhenAwayTeamWonOnHalfTimeAndWonInTheEnd() {
+
+        return matches.stream()
+                .filter(match -> getMatchPredicate(match, AWAY_TEAM_WIN, AWAY_TEAM_WIN))
+                .count();
+    }
+
+    private long countWhenAwayTeamWonOnHalfTimeAndDrewInTheEnd() {
+
+        return matches.stream()
+                .filter(match -> getMatchPredicate(match, AWAY_TEAM_WIN, DRAW))
+                .count();
+    }
+
+    private long countWhenAwayTeamWonOnHalfTimeAndHomeTeamWonInTheEnd() {
+
+        return matches.stream()
+                .filter(match -> getMatchPredicate(match, AWAY_TEAM_WIN, HOME_TEAM_WIN))
+                .count();
+    }
+
+    private boolean getMatchPredicate(Match match, String halfTimeOutcome, String finalOutcome) {
+        return match.getHalfTimeOutcome().equals(halfTimeOutcome) && match.getFinalOutcome().equals(finalOutcome);
     }
 }
--- backend/src/main/java/viewmodel/MatchDetailOutcomeView.java
+++ backend/src/main/java/viewmodel/MatchDetailOutcomeView.java
@@ -1,4 +1,77 @@
 package viewmodel;
 
 public class MatchDetailOutcomeView implements ViewModel {
+    
+    private long homeTeamWonOnHalfTimeAndWonInTheEnd;
+    private long homeTeamWonOnHalfTimeAndDrewInTheEnd;
+    private long homeTeamWonOnHalfTimeAndLostInTheEnd;
+    private long halfTimeWasDrawAndHomeTeamWonInTheEnd;
+    private long halfTimeWasDrawAndWasDrawInTheEnd;
+    private long halfTimeWasDrawAndAwayTeamWonInTheEnd;
+    private long awayTeamWonOnHalfTimeAndWonInTheEnd;
+    private long awayTeamWonOnHalfTimeAndDrewInTheEnd;
+    private long awayTeamWonOnHalfTimeAndHomeTeamWonInTheEnd;
+
+    public MatchDetailOutcomeView(long homeTeamWonOnHalfTimeAndWonInTheEnd, long homeTeamWonOnHalfTimeAndDrewInTheEnd, long homeTeamWonOnHalfTimeAndLostInTheEnd, long halfTimeWasDrawAndHomeTeamWonInTheEnd, long halfTimeWasDrawAndWasDrawInTheEnd, long halfTimeWasDrawAndAwayTeamWonInTheEnd, long awayTeamWonOnHalfTimeAndWonInTheEnd, long awayTeamWonOnHalfTimeAndDrewInTheEnd, long awayTeamWonOnHalfTimeAndHomeTeamWonInTheEnd) {
+        this.homeTeamWonOnHalfTimeAndWonInTheEnd = homeTeamWonOnHalfTimeAndWonInTheEnd;
+        this.homeTeamWonOnHalfTimeAndDrewInTheEnd = homeTeamWonOnHalfTimeAndDrewInTheEnd;
+        this.homeTeamWonOnHalfTimeAndLostInTheEnd = homeTeamWonOnHalfTimeAndLostInTheEnd;
+        this.halfTimeWasDrawAndHomeTeamWonInTheEnd = halfTimeWasDrawAndHomeTeamWonInTheEnd;
+        this.halfTimeWasDrawAndWasDrawInTheEnd = halfTimeWasDrawAndWasDrawInTheEnd;
+        this.halfTimeWasDrawAndAwayTeamWonInTheEnd = halfTimeWasDrawAndAwayTeamWonInTheEnd;
+        this.awayTeamWonOnHalfTimeAndWonInTheEnd = awayTeamWonOnHalfTimeAndWonInTheEnd;
+        this.awayTeamWonOnHalfTimeAndDrewInTheEnd = awayTeamWonOnHalfTimeAndDrewInTheEnd;
+        this.awayTeamWonOnHalfTimeAndHomeTeamWonInTheEnd = awayTeamWonOnHalfTimeAndHomeTeamWonInTheEnd;
+    }
+
+    public long getHomeTeamWonOnHalfTimeAndWonInTheEnd() {
+        return homeTeamWonOnHalfTimeAndWonInTheEnd;
+    }
+
+    public long getHomeTeamWonOnHalfTimeAndDrewInTheEnd() {
+        return homeTeamWonOnHalfTimeAndDrewInTheEnd;
+    }
+
+    public long getHomeTeamWonOnHalfTimeAndLostInTheEnd() {
+        return homeTeamWonOnHalfTimeAndLostInTheEnd;
+    }
+
+    public long getHalfTimeWasDrawAndHomeTeamWonInTheEnd() {
+        return halfTimeWasDrawAndHomeTeamWonInTheEnd;
+    }
+
+    public long getHalfTimeWasDrawAndWasDrawInTheEnd() {
+        return halfTimeWasDrawAndWasDrawInTheEnd;
+    }
+
+    public long getHalfTimeWasDrawAndAwayTeamWonInTheEnd() {
+        return halfTimeWasDrawAndAwayTeamWonInTheEnd;
+    }
+
+    public long getAwayTeamWonOnHalfTimeAndWonInTheEnd() {
+        return awayTeamWonOnHalfTimeAndWonInTheEnd;
+    }
+
+    public long getAwayTeamWonOnHalfTimeAndDrewInTheEnd() {
+        return awayTeamWonOnHalfTimeAndDrewInTheEnd;
+    }
+
+    public long getAwayTeamWonOnHalfTimeAndHomeTeamWonInTheEnd() {
+        return awayTeamWonOnHalfTimeAndHomeTeamWonInTheEnd;
+    }
+
+    @Override
+    public String toString() {
+        return "MatchDetailOutcomeView{" +
+                "homeTeamWonOnHalfTimeAndWonInTheEnd=" + homeTeamWonOnHalfTimeAndWonInTheEnd +
+                ", homeTeamWonOnHalfTimeAndDrewInTheEnd=" + homeTeamWonOnHalfTimeAndDrewInTheEnd +
+                ", homeTeamWonOnHalfTimeAndLostInTheEnd=" + homeTeamWonOnHalfTimeAndLostInTheEnd +
+                ", halfTimeWasDrawAndHomeTeamWonInTheEnd=" + halfTimeWasDrawAndHomeTeamWonInTheEnd +
+                ", halfTimeWasDrawAndWasDrawInTheEnd=" + halfTimeWasDrawAndWasDrawInTheEnd +
+                ", halfTimeWasDrawAndAwayTeamWonInTheEnd=" + halfTimeWasDrawAndAwayTeamWonInTheEnd +
+                ", awayTeamWonOnHalfTimeAndWonInTheEnd=" + awayTeamWonOnHalfTimeAndWonInTheEnd +
+                ", awayTeamWonOnHalfTimeAndDrewInTheEnd=" + awayTeamWonOnHalfTimeAndDrewInTheEnd +
+                ", awayTeamWonOnHalfTimeAndHomeTeamWonInTheEnd=" + awayTeamWonOnHalfTimeAndHomeTeamWonInTheEnd +
+                '}';
+    }
 }
