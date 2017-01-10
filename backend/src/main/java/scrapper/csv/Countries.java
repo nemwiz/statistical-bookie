37c1fae66a2e5d86854a48b4cddf0ac35ed64764
nemwiz <s3ptease@yahoo.com>
2017-01-10 20:41:45 +0100

    Countries and country code enums

---
+++ backend/src/main/java/scrapper/csv/Countries.java
@@ -0,0 +1,15 @@
+package scrapper.csv;
+
+public enum Countries {
+    England,
+    Scotland,
+    Germany,
+    Italy,
+    Spain,
+    France,
+    Netherlands,
+    Belgium,
+    Portugal,
+    Turkey,
+    Greece
+}
