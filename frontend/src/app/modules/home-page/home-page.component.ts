f333b65ff2aef2a0134bd396ce11b8eb477c6b74
nemwiz <s3ptease@yahoo.com>
2017-07-08 08:47:31 +0200

    Added SemanticUI

--- frontend/src/app/modules/home-page/home-page.component.ts
+++ frontend/src/app/modules/home-page/home-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare var jQuery: any;
 
 @Component({
   selector: 'home-page',
@@ -10,6 +11,35 @@ export class HomePageComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    jQuery('.ui.search').search({
+        source: [
+          { title: 'Andorra' },
+          { title: 'United Arab Emirates' },
+          { title: 'Afghanistan' },
+          { title: 'Antigua' },
+          { title: 'Anguilla' },
+          { title: 'Albania' },
+          { title: 'Armenia' },
+          { title: 'Netherlands Antilles' },
+          { title: 'Angola' },
+          { title: 'Argentina' },
+          { title: 'American Samoa' },
+          { title: 'Austria' },
+          { title: 'Australia' },
+          { title: 'Aruba' },
+          { title: 'Aland Islands' },
+          { title: 'Azerbaijan' },
+          { title: 'Bosnia' },
+          { title: 'Barbados' },
+          { title: 'Bangladesh' },
+          { title: 'Belgium' },
+          { title: 'Burkina Faso' },
+          { title: 'Bulgaria' },
+          { title: 'Bahrain' },
+          { title: 'Burundi' }
+        ]
+      })
+    ;
   }
 
 }
