69216696ef979c509ce189c3e832fd8916eadb5d
nemwiz <s3ptease@yahoo.com>
2017-07-08 18:16:56 +0200

    Home page fixtures list

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,6 +5,10 @@ import { AppComponent } from './app.component';
 import { HomePageComponent } from './modules/home-page/home-page.component';
 import {AppRoutes} from "./common/routes";
 import {RouterModule} from "@angular/router";
+import {FixturesService} from "./services/fixtures.service";
+import {LeaguesService} from "./services/leagues.service";
+import {MatchService} from "./services/match.service";
+import {HttpModule} from "@angular/http";
 
 @NgModule({
   declarations: [
@@ -13,9 +17,10 @@ import {RouterModule} from "@angular/router";
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     RouterModule.forRoot(AppRoutes.routes)
   ],
-  providers: [],
+  providers: [FixturesService, LeaguesService, MatchService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
