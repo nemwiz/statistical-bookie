6a3e780175667d983d0a295a6ecf20abe164910a
nemwiz <s3ptease@yahoo.com>
2017-07-12 07:02:36 +0200

    Implemented CORS on backend

--- backend/src/main/java/StatsBookie.java
+++ backend/src/main/java/StatsBookie.java
@@ -10,9 +10,14 @@ import healthchecks.DatabaseHealthCheck;
 import io.dropwizard.Application;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+import org.eclipse.jetty.servlets.CrossOriginFilter;
 import resource.FixturesResource;
 import resource.LeaguesResource;
 import resource.NumberOfGoalsPerMatchResource;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
+import java.util.EnumSet;
 
 public class StatsBookie extends Application<StatsBookieConfiguration> {
 
@@ -58,6 +63,18 @@ public class StatsBookie extends Application<StatsBookieConfiguration> {
         environment.jersey().register(numberOfGoalsPerMatchResource);
         environment.jersey().register(fixturesResource);
         environment.jersey().register(leaguesResource);
+
+        // Enable CORS headers
+        final FilterRegistration.Dynamic cors =
+                environment.servlets().addFilter("CORS", CrossOriginFilter.class);
+
+        // Configure CORS parameters
+        cors.setInitParameter("allowedOrigins", "*");
+        cors.setInitParameter("allowedHeaders", "X-Requested-With,Content-Type,Accept,Origin");
+        cors.setInitParameter("allowedMethods", "OPTIONS,GET,PUT,POST,DELETE,HEAD");
+
+        // Add URL mapping
+        cors.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
 
         environment.jersey().setUrlPattern(BASE_URL);
     }
