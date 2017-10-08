38b0f5e572e83853b1c99da442e183219d37d5db
nemwiz <s3ptease@yahoo.com>
2017-10-08 11:53:18 +0200

    Fetch data when back online

--- frontend/src/app/modules/matches/home-page/home-page.component.ts
+++ frontend/src/app/modules/matches/home-page/home-page.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from "@angular/core";
+import {ChangeDetectorRef, Component, OnDestroy, OnInit} from "@angular/core";
 import {LeaguesService} from "../../../services/leagues.service";
 import {League} from "../../../interfaces/league";
 
@@ -7,7 +7,7 @@ import {League} from "../../../interfaces/league";
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss']
 })
-export class HomePageComponent implements OnInit {
+export class HomePageComponent implements OnInit, OnDestroy {
 
   isLoading: boolean = true;
   leagues: League[] = [];
@@ -15,23 +15,47 @@ export class HomePageComponent implements OnInit {
   shouldShowErrorMessage: boolean = false;
   errorMessage: string;
 
-  constructor(private leaguesService: LeaguesService) {
+  onOnline = () => {
+    this.fetchData();
+  };
+
+  constructor(private leaguesService: LeaguesService,
+              private changeDetector: ChangeDetectorRef) {
   }
 
   ngOnInit() {
 
+    document.addEventListener('online', this.onOnline);
+
+    if (navigator['connection'].type === 'none') {
+      this.errorMessage = 'noInternetConnection';
+      this.shouldShowErrorMessage = false;
+      this.isLoading =  false;
+    } else {
+      this.fetchData();
+    }
+  }
+
+  private fetchData() {
     this.leaguesService.getAllLeagues()
       .subscribe((leagues) => {
-        this.leagues = leagues.sort((a, b) => { return a.id - b.id});
+        this.leagues = leagues.sort((a, b) => {
+          return a.id - b.id
+        });
         if (this.leagues.length === 0) {
           this.errorMessage = 'noDataAvailable'
         }
         this.isLoading = false;
+        this.changeDetector.detectChanges();
       }, error => {
         this.shouldShowErrorMessage = true;
         this.errorMessage = 'serverError';
         this.isLoading = false;
       });
+  }
+
+  ngOnDestroy(): void {
+    document.removeEventListener('online', this.onOnline);
   }
 
 }
