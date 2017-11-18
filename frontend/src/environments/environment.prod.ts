3c0f38c67a01bcb4fd76f89f4cc6c7f50a2179c4
nemwiz <s3ptease@yahoo.com>
2017-11-18 13:43:46 +0100

    Added ad banner key

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://stats-bookie-backend.herokuapp.com',
-  adMobId: 'dummyKey'
+  adMobId: 'ca-app-pub-9895015804650159/2107792472'
 };
