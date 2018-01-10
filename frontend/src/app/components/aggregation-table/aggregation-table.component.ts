07c1beb54a91a91b230cfc140643d6fd7ba02ca8
nemwiz <s3ptease@yahoo.com>
2018-01-10 21:20:17 +0100

    Added new translation keys

--- frontend/src/app/components/aggregation-table/aggregation-table.component.ts
+++ frontend/src/app/components/aggregation-table/aggregation-table.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'aggregation-table',
@@ -21,8 +21,20 @@ export class AggregationTableComponent implements OnInit {
     return percentage >= 80 ? 'green-row' : percentage >= 60 ? 'yellow-row' : '';
   }
 
-  calculatePercentage(aggregationValue: number) : number {
+  calculatePercentage(aggregationValue: number): number {
     return aggregationValue / this.matchCount * 100;
+  }
+
+  getTranslationValue(gameKey: string): string {
+    return gameKey.substr(gameKey.indexOf('-') + 1, gameKey.length);
+  }
+
+  getTranslationKey(gameKey: string): string {
+    return `${gameKey.substr(0, gameKey.indexOf('-'))}Key`;
+  }
+
+  isSpecificTranslationKeyNeeded(gameKey: string): boolean {
+    return gameKey.includes('1stHalf') || gameKey.includes('2ndHalf');
   }
 
 }
